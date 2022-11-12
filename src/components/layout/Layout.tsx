358a31718479feaa078aa8f565ca03668b03ab0e
kielllll <eatarranza@gmail.com>
2022-11-12 09:18:54 +0800

    feat: setup layout and providers

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,15 @@
+import Header from '@/components/layout/Header';
 import * as React from 'react';
 
+import { PreloadProvider } from '@/context/PreloadContext';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // Put Header or Footer Here
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      <PreloadProvider>
+        <div id='skip-nav'>{children}</div>
+      </PreloadProvider>
+    </>
+  );
 }
